1a37f6c85b32987d958e568976b06ed46e04490e
Yongki JayZ An <yxa8247@rit.edu>
2017-02-20 19:50:34 -0500

    updated GPIO init, but not sure this is correct

--- GPIO.c
+++ GPIO.c
@@ -2,12 +2,14 @@
 #include "GPIO.h"
 
 /*
-* Initialize GPIO Port A into Alternate Function Mode
+* Initialize GPIO Port A and B into Output Mode
 */
 void GPIO_Init(void) {
-	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN; // Enable GPIO Port A
-  GPIOA->MODER &= ~3;                  // clear MODER Bytes
-	GPIOA->MODER |= 0x2; 								// Set Port A to Alternate Function Mode
-	GPIOA->AFR[0] |= 0x1;								// Assign AF 1 Register for timer 2 connection
-	//GPIOA->PUPDR |= 0x2;									// Don't know what this does yet
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN; 	// Enable GPIO Port A
+  GPIOA->MODER &= ~3;                  	// clear MODER Bytes
+	GPIOA->MODER |= 0x1; 									// Set Port A to Output Mode
+	//GPIOA->OTYPER &= ~0x10;								// set PA 0 and 1 to be push-pull (reset state) type ?
+	//GPIOA->OSPEEDR &= ~0x10;							// set PA 0 and 1 to be low speed ?
+	//GPIOA->PUPDR |= 0x2;									// pull down on PA0 ?
+	//GPIOA->ODR |= 0x0;										// set PA0 output data register ?
 }
