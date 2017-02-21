b6db2072a87a91f6cb86914887c3a7eca29d411e
Yongki JayZ An <yxa8247@rit.edu>
2017-02-20 19:00:14 -0500

    added basic template of the projecT

--- main.c
+++ main.c
@@ -1,3 +1,26 @@
 /*
 * Author: Yongki (Jay) / Gowtham
 */
+/* include provided start up code*/
+#include "stm32l476xx.h"
+#include "SysClock.h"
+#include "LED.h"
+#include "UART.h"
+/* include useful C libraries */
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+/* define good numbers */
+#define BufferSize 100
+
+/* declare variables here */
+uint8_t buffer[BufferSize];
+
+int main(void){
+	
+	System_Clock_Init();		// Switch System Clock = 80 MHz
+	UART2_Init();
+
+	return 0;
+}
