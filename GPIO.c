c7842f8c8df933d721411ad7c37a00f490ad18e7
Yongki JayZ An <yxa8247@rit.edu>
2017-02-25 19:11:38 -0500

    finished GPIO/Timer initialization and little more fix on UI

--- GPIO.c
+++ GPIO.c
@@ -9,27 +9,18 @@ void GPIO_Init(void) {
   GPIOA->MODER &= ~3;                   // clear MODER Bytes
 	GPIOA->MODER |= 0x2; 									// Set Port A to Alternate Function Mode
 	GPIOA->AFR[0] |= 0x1;									// Assign AF 1 Register for timer 2 connection
-	GPIOA->PUPDR |= 0x2;									// 
+	GPIOA->PUPDR |= 0x2;									// Set PA0 to Pull-Down mode
 }
 
 void TIM_Init(void) {
 	RCC->APB1ENR1 |= RCC_APB1ENR1_TIM2EN; // Enable Timer 2 Clock
 	TIM2->PSC = 79;												// Set 80Mhz CPU Pre-Scalar
-	TIM2->EGR |= 0x01;										// Force update by setting EGR bit
 	TIM2->CCER &= 0x1;										// Disable Timer 2 output register
-	
-	TIM2->CCMR1 &= 0x0;										// The channel is now output
-	//TIM2[TIM_CCMR1_CC1S] = 0;						// The channel is now output
-	TIM2->CCMR1  |= 0x100;								// Set OC1PE (Output compare 1 preload enable)
-	//TIM2->[TIM_CCMR1_OC1PE] = 1;				// Set OC1PE (output Compare 1 Preload Enable)
+	TIM2->CCMR1 &= 0x00;									// Clear CCMR1 CC1S bit to set output mode
+	TIM2->CCMR1 |= TIM_CCMR1_OC1PE;				// Set OC1PE (output Compare 1 Preload Enable)
 	TIM2->CR1 |= TIM_CR1_ARPE;						// Set the Auto-Reload Preload Enable bit
-	// or TIM2->CR1 |= 0x10000000;				// Set the Auto-Reload Preload Enable bit
-	
-	TIM2->CCER |= 0x1;										// Enable Timer 2 output register
-	TIM2->EGR |= 0x01;										// Force update by setting EGR bit
-	
 	TIM2->ARR = (20*1000);								// Set Auto Reload Register of 20 milli-seconds period
-	TIM2->CCR1 = (0.38*1000);						// Set the pulse width of 380 micro-seconds in CCR1 Register
+	TIM2->CCR1 = (380);										// Set the pulse width of 380 micro-seconds in CCR1 Register
 	TIM2->EGR |= 0x01;										// Force update by setting EGR bit
-	
+	TIM2->CCER |= 0x1;										// Enable Timer 2 output register
 }
--- UI.c
+++ UI.c
@@ -8,6 +8,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define PROMPT_SIZE 7
+
 // uint8_t buffer[BufferSize]; //(already declared in main.c) 
 uint8_t uiBuffer[BufferSize];
 
@@ -28,7 +30,7 @@ void left_servo( int *left_flag)
 	{
 		// continue servo commands here
 	}
-void 	noop_servo(int *noop_flag)
+void noop_servo(int *noop_flag)
 	{
 		// pause servo commands here
 	}
@@ -38,31 +40,31 @@ void reset_servo( int *reset_flag)
 	} 	
 void user_prompt() {			
 	// start writing user command code here
-	int pause_flag, continue_flag, right_flag, left_flag , noop_flag, reset_flag ;
-	int i;
+	int pause_flag, continue_flag, right_flag, left_flag , noop_flag, reset_flag, i;
 	char rxbyte[2];
-	const char *prompts[7];
+	const char *prompts[PROMPT_SIZE];
 	prompts[0] = "Welcome to the Project 2, and here are the instructions\r\n";
-	prompts[1] = "1. Press P or p for pausing the execution\r\n";
-	prompts[2] = "2. Press C or c to continue the recipe\r\n";
-	prompts[3] = "3. Press R or r to move 1 position to the right if possible\r\n";
-	prompts[4] = "4. Press L or l Move 1 position to the left if possible";
-	prompts[5] = "5. Press N or n for No-operation\r\n";
-	prompts[6] = "6. press B or b to restart the reciepe\r\n";
+	prompts[1] = "1.Press P or p for pausing the execution\r\n";
+	prompts[2] = "2.Press C or c to continue the recipe\r\n";
+	prompts[3] = "3.Press R or r to move 1 position to the right if possible\r\n";
+	prompts[4] = "4.Press L or l Move 1 position to the left if possible\r\n";
+	prompts[5] = "5.Press N or n for No-operation\r\n";
+	prompts[6] = "6.press B or b to restart the reciepe\r\n";
 	
-	for (i=0; i<sizeof(prompts); i++) {
+	for (i=0; i<PROMPT_SIZE; i++) {
 		// print all 6 prompt lines for user tutorial
-		USART_Write( USART2, (uint8_t *)prompts[i], sizeof(prompts[i]));
+		USART_Write( USART2, (uint8_t *)prompts[i], strlen(prompts[i]));
 	}
 	
 	while(1) { // run forever
-		char the_prompt[] = "Type two commands for asynchronous Servo running\r\n";
+		char the_prompt[] = "\r\nType two commands for asynchronous Servo running\r\n";
 		USART_Write( USART2, (uint8_t *)the_prompt, sizeof(the_prompt));
 		
 		for(i=0;i<2;i++) {
 			// first, run double UART read wait
 			rxbyte[i]=USART_Read(USART2);
 		}
+		strcat(rxbyte, "\r\n"); //add line break to the rxbyte
 		// then print the data in the rxbyte
 		USART_Write(USART2, (uint8_t *)rxbyte, sizeof(rxbyte));
 		
